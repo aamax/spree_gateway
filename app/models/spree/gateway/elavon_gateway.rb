dccad5719f2da08c946de490bc6f10ff3d140f5c
Allen Maxwell <aamax@xmission.com>
2013-06-11 21:22:50 -0600

    added elavon_gateway file for new gateway and inserted into engine initializer

---
+++ app/models/spree/gateway/elavon_gateway.rb
@@ -0,0 +1,27 @@
+module Spree
+  class Gateway::ElavonGateway < Gateway
+    #preference :login, :string
+    #preference :password, :string
+    #
+    #attr_accessible :preferred_login, :preferred_password
+    #
+    #def provider_class
+    #  ActiveMerchant::Billing::AuthorizeNetGateway
+    #end
+    #
+    #def options_with_test_preference
+    #  options_without_test_preference.merge(:test => self.preferred_test_mode)
+    #end
+    #
+    #alias_method_chain :options, :test_preference
+
+    preference :login, :string
+    preference :password, :string
+    preference :user, :string
+
+    def provider_class
+      ActiveMerchant::Billing::ElavonGateway
+    end
+
+  end
+end
